ec8a7316e518280741b31b7d560cef75ab0ffb90
Ilkhomidin Bakhoraliev <bakhoraliev.ilkhomidin@proton.me>
2023-09-30 04:17:38 +0500

    Add more documentation for `Service` object

--- domain/service.py
+++ domain/service.py
@@ -8,9 +8,14 @@ class Service(TypedDict):
     """
     Service is a value object that represents a service that can be
         purchased.
+
+    Contains data about the service, such as its name, description, image URL for cover,
+        price, and time it takes to complete the service.
     """
 
     name: str
+    description: str
+    image_url: str
     price: Money
     time: datetime.timedelta
 
